83206e5524120232afffb310b6c673207e9ac30f
Deepak Kumar Yadav <98113881+2003deepak@users.noreply.github.com>
2024-07-19 12:51:27 +0530

    Update Login.jsx

--- Chat API/src/Pages/Login.jsx
+++ Chat API/src/Pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
   const handleSubmit = async (name,email) => {
     
     try {
-      const response = await axios.post('http://localhost:8000/login', {
+      const response = await axios.post('https://chat-api-blond.vercel.app//login', {
         name,
         email,
       });
